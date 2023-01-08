2932fa19038704f09e16410308c889921500838d
stanton119 <stanton1191@gmail.com>
2023-01-08 23:21:22 +0000

    convert markdown action

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -3,24 +3,10 @@
 echo $1
 echo $2
 
-# files=$(git diff --name-only --diff-filter=ACMRT $1 $2 | grep .ipynb)
-# files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
 # ignore deleted files, filter for .ipynb only, filter for last 10 commits
-files=$(git diff --name-only --diff-filter=d HEAD~10 HEAD | grep .ipynb)
+files=$(git diff --name-only --diff-filter=d $1 $2 | grep .ipynb)
 for i in $files;
 do
    echo $i
    jupyter nbconvert --to markdown $i
 done
-
-
-# files=$(git diff --name-only --diff-filter=ACMRT 6dc1e49e27d6cb9c02d635a0198fa7b17adb741f | grep .ipynb)
-# github.event.pull_request.base.sha=$('6dc1e49e27d6cb9c02d635a0198fa7b17adb741f')
-# files=$(git diff --name-only --diff-filter=ACMRT ${{ github.event.pull_request.base.sha }} ${{ github.sha }} | grep .ipynb)
-# for i in $files;
-# do
-#    jupyter nbconvert --to markdown $i
-# done
-
-
-# git diff --name-only --diff-filter=ACMRT ${{ github.event.pull_request.base.sha }} ${{ github.sha }} | grep .css$ | xargs.
